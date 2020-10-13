d5367911aae56799203dc40878a207ef2f6f8fe9
wxy-133 <w15279621208@163.com>
2020-10-14 00:26:25 +0800

    '10/14划分目录结构'

---
+++ src/view/home/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <h2>home</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: [''],
+  data() {
+    return {
+
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {}
+
+}
+</script>
+
+<style  scoped>
+
+</style>
---
+++ src/view/my/My.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <h2>my</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'My',
+  props: [''],
+  data() {
+    return {
+
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {}
+
+}
+</script>
+
+<style  scoped>
+
+</style>
