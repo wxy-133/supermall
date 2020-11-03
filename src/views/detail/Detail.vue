0fab20cc1cc4a332fe05d92b2c31115b4b019747
wxy-133 <w15279621208@163.com>
2020-11-03 23:32:44 +0800

    跳转详情页携带id

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>111</div>
+    <div>{{iid}}</div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'Detail',
+    props:[''],
+    data () {
+      return {
+       iid:null
+      };
+    },
+
+    components: {},
+    created(){
+        this.iid = this.$route.params.iid
+    }
+
+  }
+
+</script>
+<style scoped>
+
+</style>
